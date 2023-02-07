e86e9171bf6e2a4a5409e48db0c63ff4b8d11a14
Logan Volkers <logan@referralsaasquatch.com>
2023-02-07 15:25:54 -0800

    Update polyfill.ts

--- polyfill.ts
+++ polyfill.ts
@@ -11,20 +11,22 @@ These are similar to the standard browser events:
   - beforeunload
 
 **/
-function wrapHistoryFunction(functionName: string, eventName: string) {
-  const orig = (window.history as any)[functionName];
-  return function(...args: any[]) {
-    const beforeEvent = new Event("before" + eventName, {
-      cancelable: true,
-      bubbles: true
-    });
-    const passed = window.dispatchEvent(beforeEvent);
-    if (passed) {
-      const returnValue = orig.apply(window.history, args);
-      const event = new Event(eventName);
-      window.dispatchEvent(event);
-    }
-  };
+export default function polyfill(){
+  function wrapHistoryFunction(functionName: string, eventName: string) {
+    const orig = (window.history as any)[functionName];
+    return function(...args: any[]) {
+      const beforeEvent = new Event("before" + eventName, {
+        cancelable: true,
+        bubbles: true
+      });
+      const passed = window.dispatchEvent(beforeEvent);
+      if (passed) {
+        const returnValue = orig.apply(window.history, args);
+        const event = new Event(eventName);
+        window.dispatchEvent(event);
+      }
+    };
+  }
+  window.history.pushState = wrapHistoryFunction("pushState", "pushstate");
+  window.history.replaceState = wrapHistoryFunction("replaceState", "replacestate");
 }
-window.history.pushState = wrapHistoryFunction("pushState", "pushstate");
-window.history.replaceState = wrapHistoryFunction("replaceState", "replacestate");
