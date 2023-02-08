4837b4168367a8ea3e741deabd90be926afe069d
Logan Volkers <logan@referralsaasquatch.com>
2023-02-07 16:21:15 -0800

    Create polyfill.ts

--- polyfill.ts
+++ polyfill.ts
@@ -10,23 +10,34 @@ These are similar to the standard browser events:
   - popstate
   - beforeunload
 
+
+  Please read: https://github.com/saasquatch/pushstate-polyfill/
+  
 **/
-export default function polyfill(){
+export default function polyfill() {
   function wrapHistoryFunction(functionName: string, eventName: string) {
     const orig = (window.history as any)[functionName];
-    return function(...args: any[]) {
+    return function (...args: any[]) {
       const beforeEvent = new Event("before" + eventName, {
         cancelable: true,
-        bubbles: true
+        bubbles: true,
       });
-      const passed = window.dispatchEvent(beforeEvent);
+      const notCancelled = window.dispatchEvent(beforeEvent);
+      let passed = true;
+      if (!notCancelled) {
+        passed = window.confirm(
+          "Are you sure you want to leave with unsaved changed?"
+        );
+      }
       if (passed) {
-        const returnValue = orig.apply(window.history, args);
-        const event = new Event(eventName);
-        window.dispatchEvent(event);
+        orig.apply(window.history, args);
+        window.dispatchEvent(new Event(eventName));
       }
     };
   }
   window.history.pushState = wrapHistoryFunction("pushState", "pushstate");
-  window.history.replaceState = wrapHistoryFunction("replaceState", "replacestate");
+  window.history.replaceState = wrapHistoryFunction(
+    "replaceState",
+    "replacestate"
+  );
 }
